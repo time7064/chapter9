dd9c65da7131c057f7d44ddeb1dd0ec309067577
time7064 <time7064@gmail.com>
2026-05-08 11:43:18 +0900

    change base url

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h1>헬로.. 테스트..</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
